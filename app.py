3690f26c4ba9adb4ea867a6fdf95c46bc5e3e2c4
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-06-12 00:34:53 +1000

    merge ready files from branch hiu

---
+++ app.py
@@ -0,0 +1,42 @@
+import json
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from bson.json_util import dumps, loads
+import data_clean
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/avocado_data")
+
+@app.route("/")
+
+def home ():
+    # avocado_data = mongo.db.collection.find_one()
+
+    avocado_data = mongo.db.volume_data.find()
+    list_data = list(avocado_data)
+
+    json_data = dumps(list_data, indent =2)
+
+    with open('data.json', 'w') as file:
+        file.write(json_data)
+
+    return render_template("index.html", avocado_data = avocado_data)
+
+@app.route("/fetch_data")
+def volume_file():
+    vol_data = data_clean.volume_file()
+
+    total_sold = data_clean.total_file()
+
+    # mongo.db.collection.update_one({}, {"$set": vol_data}, upsert=True)
+    result = mongo.db.volume_data.delete_many({})
+    mongo.db.volume_data.insert_many(vol_data)
+    mongo.db.volume_data.insert_many(total_sold)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ data_clean.py
@@ -0,0 +1,116 @@
+# Import Dependencies
+import pandas as pd
+import os, fnmatch
+# from splinter import Browser
+# from bs4 import BeautifulSoup as bs
+import datetime as dt
+# from webdriver_manager.chrome import ChromeDriverManager
+
+def file_search():
+    # Searching for csv filenames
+
+    for root, dirs, files in os.walk("resources", topdown=False):
+        resourcesVFiles = []
+        resourcesRFiles = []
+        resourcesTotFiles = []
+        for name in files:
+            if os.path.join(name).startswith("Volume"):
+                resourcesVFiles.append(os.path.join(root, name))
+            elif os.path.join(name).rfind("total"):
+                resourcesTotFiles.append(os.path.join(root, name))
+            else:
+                resourcesRFiles.append(os.path.join(root, name))
+    
+    # Load all relevant filenames and path to a dictionary
+    resourcesFile = {
+        "volume_data": resourcesVFiles,
+        "region_data": resourcesRFiles,
+        "sales_tot_data": resourcesTotFiles
+    }
+    return  resourcesFile
+
+
+def volume_file():
+
+    resourcesVFiles = file_search()['volume_data']
+
+    # List to store df names
+    volumedf_list = []
+
+    # Load Volume into dfs
+    for i in range(len(resourcesVFiles)):
+        file = resourcesVFiles[i]
+        i = i + 1
+        variable_name = f"vol_{i}"
+        globals()[variable_name] =  pd.read_csv(file)
+        volumedf_list.append(variable_name)
+
+    # Union Join dfs
+    volumedf_list = [vol_1, vol_2, vol_3]
+    volume = pd.concat(volumedf_list, axis=0, ignore_index=True)
+
+    # Replace N/A with zeros
+    volume.fillna(0)
+
+    for col in volume.columns:
+        if not (col == 'Week' or col == 'Status' or col == 'Year'):
+            try:
+                volume[col] = volume[col].str.replace(",", "").astype(float)
+            except AttributeError:
+                print(f'skipped {col}')
+
+    volume_transformed = volume.fillna(0)
+
+    volume_transformed['WEDate'] = pd.to_datetime(volume_transformed.Week)
+
+    actual_vol_temp = volume_transformed.loc[(volume_transformed["Status"] == "Actual"), :]
+    actual_vol = actual_vol_temp.to_dict("records")
+
+    return actual_vol
+
+def total_file():
+
+    # Extract filenames and Path or csv files relating to volume unit
+
+    resourcesTotFiles = file_search()['sales_tot_data']
+
+    # numfile = len(resourcesTotFiles)
+    totaldf_list = []
+    
+    # Load Volume into dfs
+    for i in range(len(resourcesTotFiles)):
+        file = resourcesTotFiles[i]
+        i = i + 1
+        variable_name = f"tot_{i}"
+        globals()[variable_name] =  pd.read_csv(file)
+        totaldf_list.append(variable_name)
+
+    # Union Join dfs
+    totaldf_list = [tot_1, tot_2, tot_3]
+    total_temp = pd.concat(totaldf_list, axis=0, ignore_index=True)
+
+    # Replace N/A with zeros
+    total_temp.fillna(0)
+    
+    rearrange_cols_df = total_temp[['Geography',
+        'Timeframe',
+        'Current Year Week Ending',
+        'Type',
+        'ASP Current Year',
+        '4046 Units',
+        '4225 Units',
+        '4770 Units',
+        'Bulk GTIN',
+        'TotalBagged Units',
+        'Total Bulk and Bags Units']]
+
+    # Change dates columns to date 
+    rearrange_cols_df['Current Year Week Ending'] = pd.to_datetime(rearrange_cols_df['Current Year Week Ending'])
+    rearrange_cols_df['Year']=rearrange_cols_df['Current Year Week Ending'].dt.year
+
+    # Rename columns
+    rearrange_cols_df.columns = ['City', 'Timeframe', 'Weekly Reporting Date', 'Produce Type', 'Average Avocado Price Year', 'Small/Medium (4046) Units', 'Large (4225) Units', 'Extra Large (4770) Units', 'Bulk GTIN', 'Bagged Units', 'Total Units', 'Year'] 
+
+    total_sold = rearrange_cols_df.to_dict('records')
+
+    return total_sold
