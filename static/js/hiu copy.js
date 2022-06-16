3e7a8f13002531c0ef6efd20800eb48f166b4955
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-06-16 19:51:28 +1000

    update

--- static/js/hiu copy.js
+++ static/js/hiu copy.js
@@ -1,9 +1,22 @@
-  d3.json("http://192.168.1.121:8000/avocado/volume").then(function(volume_data, err) {
+  d3.json("/avocado/volume").then(function(volume_data, err) {
     if (err) throw err;
-  
-    volume_data.forEach(function(data){
-		console.log(data)
 
-	}
-	)
+	// var dateFormat = d3.time.format("%Y-%m-%d");
+    volume_data.forEach(function(d){
+		// d["WEDate"] = dateFormat.parse(d["WEDate"]);
+		d["WEDate"].setDate(1);
+		d["California"] = +d["California"];
+		d["Chile"] = +d["Chile"];
+		d["Colombia"] = +d["Colombua"];
+		d["Dominican Republic"] = +d["Dominican Republic"];
+		d["Mexico"] = +d["Mexico"];
+		d["Peru"] = +d["Peru"];
+		d["Total Volume"] = +d["Total Volume"];
+		d['Year'] = +d['Year'];
+		
+	// });
+	})
+		var vol_2020 = volume_data.filter(d => d['Year'] == '2020')
+		console.log(vol_2020)
+
 })
