68ccabef2494de88fa7ad9b3bce11a500281fab1
kramana193 <ramana_kumar193@yahoo.com>
2022-06-17 15:04:24 +1000

    update

--- Ramana/js/Ramana_volume.js
+++ Ramana/js/Ramana_volume.js
@@ -139,29 +139,81 @@ function renderChart(series){
 
 
 
+// JS Drop down trial code  
+// $(document).ready(function() {
+//     $('#generate').click(function() {
+//         var values = ["dog", "cat", "parrot", "rabbit"];
  
-$(document).ready(function() {
-    $('#generate').click(function() {
-        var values = ["dog", "cat", "parrot", "rabbit"];
+//         $('#container')
+//         .append(
+//             $(document.createElement('label')).prop({
+//                 for: 'pets'
+//             }).html('Choose your pets: ')
+//         )
+//         .append(
+//             $(document.createElement('select')).prop({
+//                 id: 'pets',
+//                 name: 'pets'
+//             })
+//         )
  
-        $('#container')
-        .append(
-            $(document.createElement('label')).prop({
-                for: 'pets'
-            }).html('Choose your pets: ')
-        )
-        .append(
-            $(document.createElement('select')).prop({
-                id: 'pets',
-                name: 'pets'
-            })
-        )
- 
-        for (const val of values) {
-            $('#pets').append($(document.createElement('option')).prop({
-                value: val,
-                text: val.charAt(0).toUpperCase() + val.slice(1)
-            }))
-        }
-    })
+//         for (const val of values) {
+//             $('#pets').append($(document.createElement('option')).prop({
+//                 value: val,
+//                 text: val.charAt(0).toUpperCase() + val.slice(1)
+//             }))
+//         }
+//     })
+// });
+
+function makeTrace(i) {
+    return {
+        y: Array.apply(null, Array(10)).map(() => Math.random()),
+        line: {
+            shape: 'spline' ,
+            color: 'red'
+        },
+        visible: i === 0,
+        name: 'Data set ' + i,
+    };
+}
+
+Plotly.newPlot('myDiv', [0, 1, 2, 3].map(makeTrace), {
+    updatemenus: [{
+        y: 0.8,
+        yanchor: 'top',
+        buttons: [{
+            method: 'restyle',
+            args: ['line.color', 'red'],
+            label: 'red'
+        }, {
+            method: 'restyle',
+            args: ['line.color', 'blue'],
+            label: 'blue'
+        }, {
+            method: 'restyle',
+            args: ['line.color', 'green'],
+            label: 'green'
+        }]
+    }, {
+        y: 1,
+        yanchor: 'top',
+        buttons: [{
+            method: 'restyle',
+            args: ['visible', [true, false, false, false]],
+            label: 'Data set 0'
+        }, {
+            method: 'restyle',
+            args: ['visible', [false, true, false, false]],
+            label: 'Data set 1'
+        }, {
+            method: 'restyle',
+            args: ['visible', [false, false, true, false]],
+            label: 'Data set 2'
+        }, {
+            method: 'restyle',
+            args: ['visible', [false, false, false, true]],
+            label: 'Data set 3'
+        }]
+    }],
 });
