750a0640c4ec0fbb5331746f0d9943d4d87e609d
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-06-19 17:00:00 +1000

    update

--- static/js/hiu copy.js
+++ static/js/hiu copy.js
@@ -1,93 +1,191 @@
-var svgWidth = 960;
-var svgHeight = 500;
+function makeResponsive() {
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 150
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-var svg = d3
-  .select("volume_3_years")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
-// var yAxis = svg.append("g")
-// 	.attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-var x = d3.scaleBand()
-	.range([0, width])
-	.padding(0.2);
-
-var xAxis = svg.append("g")
-	.attr(`transform`, `translate(0, " + height + ")`)
-
-var y = d3.scaleLinear()
-  .range([ height, 0]);
-
-var yAxis = svg.append("g")
-  .attr("class", "volume_3_years")
-
-d3.json("/av)ocado/volume").then(function(volume_data, err) {
-    if (err) throw err;
-
-	var dateFormat = d3.timeFormat("%Y-%m-%d");
-
-    volume_data.forEach(function(d){
-		d["WEDate"] = dateFormat(d["WEDate"]);
-		d["WEDate"].setDate(1);
-		d["California"] = +d["California"];
-		d["Chile"] = +d["Chile"];
-		d["Colombia"] = +d["Colombua"];
-		d["Dominican Republic"] = +d["Dominican Republic"];
-		d["Mexico"] = +d["Mexico"];
-		d["Peru"] = +d["Peru"];
-		d["Total Volume"] = +d["Total Volume"];
-		d['Year'] = +d['Year'];
-		console.log(d['WEDate'])
-	// });
-	})
-		
-		var vol_2020 = volume_data.filter(d => d['Year'] == '2020');
-		var vol_2021 = volume_data.filter(d => d['Year'] == '2021');
-		var vol_2022 = volume_data.filter(d => d['Year'] == '2022');
-		
-		chosenYear = volume_data;
-
-		console.log(chosenYear)
-	
-		x.domain(chosenYear.map(function(d) {return d['WEDate']; }))
-		xAxis.call(d3.axisBottom(x))
-
-		y.domain([0, d3.max(data, function(d) { return d.value }) ]);
-  		yAxis.transition().duration(1000).call(d3.axisLeft(y));
-
-		// Create the u variable
-		var u = svg.selectAll("rect")
-			.data(data)
-
-		u
-			.enter()
-			.append("rect") // Add a new rect for each new elements
-			.merge(u) // get the already existing elements as well
-			.transition() // and apply changes to all of them
-			.duration(1000)
-			.attr("x", function(d) { return x(d['WEDate']); })
-			.attr("y", function(d) { return y(d.['WEDate']); })
-			.attr("width", x.bandwidth())
-			.attr("height", function(d) { return height - y(d.value); })
-			.attr("fill", "#69b3a2")
-
-		// If less group in the new dataset, I delete the ones not in use anymore
-		u
-			.exit()
-			.remove()
+	var svgArea = d3.select(".volume_3_years").select("svg");
+	if (!svgArea.empty()) {
+	  svgArea.remove();
 	}
-).catch(function(error) {
-	console.log(error);
-});
+  
+  // Define SVG area dimensions
+	var svgWidth = 1160;
+	var svgHeight = 500;
+  
+	// Define the chart's margins as an object
+	var chartMargin = {
+	  top: 30,
+	  right: 30,
+	  bottom: 50,
+	  left: 30
+	};
+  
+	// Define dimensions of the chart area
+	var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+	var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+  
+	// Select body, append SVG area to it, and set the dimensions
+	var svg = d3
+	  .select(".volume_3_years")
+	  .append("svg")
+	  .attr("height", svgHeight)
+	  .attr("width", svgWidth)
+	  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+  
+  
+	d3.json("/avocado/volume").then(function(volume_data) {
+  
+	  function init() { 
+		getData();
+	  }
+  
+	  var parseTime = d3.timeParse("%d-%m-%Y");
+	  var newDateFormat = d3.timeFormat("%d-%b");
+	  var sortDate = d3.timeFormat("%Y-%m-%d");
+  
+	  // Cast the hours value to a number for each piece of tvData
+	  volume_data.forEach(function(data) {
+		data.WEDate = parseTime(data.WEDate)
+		data.sWEDate = sortDate(data.WEDate)		
+		data.WEDate = newDateFormat(data.WEDate)    
+		data.California = +data.California;
+		data.Chile = +data.Chile;
+		data.Mexico = +data.Mexico;
+		data.totalVolume = +data['Total Volume']
+	  });
+  
+	  // setup buttons
+  
+	  d3.select("#first_year").on("click", getData);
+	  d3.select("#second_year").on("click", getData);
+	  d3.select("#third_year").on("click", getData);
+	  
+	  function getData() {
+  
+		if (this.id == "first_year"){
+		  var selYear = '2020';
+		}
+		else if (this.id == "second_year") {
+		  var selYear = '2021';
+		}
+		else {
+		  var selYear = '2022';
+		}
+  
+		var chosenYear = volume_data.filter(d => d.Year == selYear);
+  
+		var subgroups_data = ['California', 'Chile', 'Colombia', 'Dominican Republic', 'Mexico', 'Peru']
+  
+		update(chosenYear)
+	  }
+  
+	  function update(chosenYear) {
+		d3.selectAll("g").remove();
+  
+		var subgroups = ['Total Volume','California', 'Chile', 'Colombia', 'Dominican Republic', 'Mexico', 'Peru']
+		var groups = d3.map(chosenYear, d => d.WEDate)
+		
+		var scaleY = 100000;
+		
+		var dataReady = subgroups.map(function(colname) {
+		  return {
+			name: colname,
+			values: chosenYear.map(function(d) {
+			  return {date: d['WEDate'], value: +d[colname]};
+			})
+		  }
+		})
+  
+		console.log(dataReady)
+		var colour = d3.scaleOrdinal()
+		.domain(dataReady)
+		.range(d3.schemeSet2)
+		// Add X axis
+		var x = d3.scaleBand()
+		  .domain(groups)
+		  .range([0, chartWidth])
+		  .padding([0.2])
+  
+		svg.append("g")
+		  .attr("transform", "translate(0," + chartHeight + ")")
+		  .call(d3.axisBottom(x).tickSize(0))
+		  .selectAll("text")
+			.attr("transform", "translate(-10,10)rotate(-90)")
+			.style("text-anchor", "end")
+			.on("click", function(d){
+			  console.log(d)
+			});
+  
+  
+		  // Add Y axis
+		// console.log(d3.max(chosenYear, data => data.Mexico / scaleY))
+		var y = d3.scaleLinear()
+		  .domain([0, (d3.max(chosenYear, data => data.Mexico / scaleY) + 70)])
+		  .range([ chartHeight, 0 ]);
+  
+		svg.append("g")
+		  .call(d3.axisLeft(y));
+  
+		var xSubgroup = d3.scaleBand()
+		  .domain(subgroups)
+		  .range([0, x.bandwidth()])
+		  .padding([0.05])
+  
+	   
+		svg.append("g")
+		  .selectAll("g")
+		  .data(chosenYear)
+		  .enter()
+		  .append("g")
+			.attr(`transform`, function(d){ 
+			  return "translate(" + x(d.WEDate) + ",0)"; 
+			})
+		  .selectAll("rect")
+		  .data(function(d) { 
+			return subgroups.map(function(key) {
+			  return {key: key, value: (d[key]/scaleY)}; 
+			}); 
+		  })
+		  .enter().append("rect")
+			.attr("x", function(d) { return xSubgroup(d.key); })
+			.attr("y", function(d) { return y(d.value); })
+			.attr("width", xSubgroup.bandwidth())
+			.attr("height", function(d) { return chartHeight - y(d.value); })
+			.attr("fill", function(d) {return colour(d.key); })
+		   
+		  svg.selectAll("myLegend")
+			.data(dataReady)
+			.enter()
+			  .append('g')
+			  .append("text")
+				.attr('x', function(d,i){ 
+				  if (d.name == 'Dominican Republic' || d.name == 'Colombia'){
+					var textsize = 560;
+				  }
+				  else {
+					console.log(d.name)
+					var textsize = 600;
+				  }
+				  return textsize + i*80
+				})
+				.attr('y', 10)
+				.text(function(d) { return d.name; })
+				.style("fill", function(d){ return colour(d.name) })
+				.style("font-size", 12)
+				.on("click", function (d){
+				  // is the element currently visible ?
+					  console.log(d)
+				  // currentOpacity = d3.selectAll("." + d.name).style("opacity")
+				  // Change the opacity: from 0 to 1 or from 1 to 0
+				  // d3.selectAll("." + d.name).transition().style("opacity", currentOpacity == 1 ? 0:1)
+  
+					})
+	  }
+	  
+	  init();
+	
+	}).catch(function(error) {
+	  console.log(error);
+	});
+  }
+  makeResponsive();
+  
+  d3.select(window).on("resize", makeResponsive);
+  
